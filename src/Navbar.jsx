99c86cb7d48becdce670da280b336b8949a0755b
Shruti Yadav <shrutiyadav26072002@gmail.com>
2023-01-31 20:19:38 +0530

    logo link updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#000000'}}>
-        <a className="navbar-brand" href="index.html" ><img src={logo} width="60" height="60" alt="..."/></a>
+      <Link to='/change-the-clap' className="nav-link" style={{ color: '#FF63B1' }}><img src={logo} width="60" height="60" alt="..."/></Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
